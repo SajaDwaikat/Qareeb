513932799763d71ecd3812cd09c7de080553bfa9
randhamdan <s12325996@stu.najah.edu>
2026-04-11 19:20:56 +0200

    done

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { router } from "expo-router";
 
-import { Redirect } from "expo-router";
-
-export default function Index() {
-  return <Redirect href="/Prop/add" />;
-
+export default function Splash() {
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/(auth)/user-type");
+    }, 1000);
+  }, []);
 
   return null;
 }
